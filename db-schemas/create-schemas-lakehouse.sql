f95e97cc4d49540adf0e2fa0a2d2e29d6447cebc
jooaobrum <jooaobrum>
2024-01-24 21:59:37 +0000

    Creating schemas of the delta lake

---
+++ db-schemas/create-schemas-lakehouse.sql
@@ -0,0 +1,10 @@
+-- Databricks notebook source
+CREATE SCHEMA IF NOT EXISTS bronze;
+
+-- COMMAND ----------
+
+CREATE SCHEMA IF NOT EXISTS silver;
+
+-- COMMAND ----------
+
+CREATE SCHEMA IF NOT EXISTS gold;
